06853561c10d7a229f392d5028758064d0b161d7
Marcos Mang <lingfengmarskey@gmail.com>
2020-04-19 21:57:12 +0900

    Add demo.

--- Example/FFUtils/ViewController.swift
+++ Example/FFUtils/ViewController.swift
@@ -7,12 +7,40 @@
 //
 
 import UIKit
+import FFUtils
 
 class ViewController: UIViewController {
 
+    var flip:FlippingView?
+    var texts = ["A", "B", "C", "D"]
+    var idx:Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        createView()
+    }
+    
+    func createView() {
+        let v = FlippingView.init(frame: CGRect.init(x: 0, y: 0, width: 100, height: 200))
+        view.addSubview(v)
+        v.text = "O"
+        flip = v
+    }
+    
+    func getIdx() -> String {
+        if idx >= texts.count {
+            idx = 0
+        }
+        let res = texts[idx]
+        idx += 1
+        return res
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        flip?.setTitle(getIdx())
+        flip?.playSoundAction = {
+            print("play sound")
+        }
     }
 
     override func didReceiveMemoryWarning() {
